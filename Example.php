2d4b45ca655b1c3992076905d7c3c27ec2027542
Miguel S. Mendoza <MiguelSMendoza@users.noreply.github.com>
2015-12-05 21:06:43 +0100

    Silex Way
    Ejemplo modificado para mostrar la nueva forma de uso

--- Example.php
+++ Example.php
@@ -1,37 +1,10 @@
 <?php
 require_once("CyberREST.php");
 
-// CALL: http://localhost/API/Contact?name=Miguel&surname=Mendoza
-
-class API extends CyberREST {
-	
-	public function processApi(){
-		$parts = $this->getRequestPartsFrom("Contact");
-		if($parts[0]=='Contact') {
-			switch($this->getRequestMethod()) {
-			case "GET":
-				$this->getContact();
-				break;
-			default:
-				$this->response('',404);
-			}
-		}
-		else 
-			$this->response('',404);
-	}
-
-	private function getContact() {
-		$params = $this->parseIncomingParams();
-		if(isset($params["name"]) && isset($params["surname"]) {
-			$contact = array("name"=>"Miguel", "surname"=>"S. Mendoza");
-			$this->response($this->encodeJSONforHTML($contact),200);
-		}
-		else
-			$this->response('',404);
-	}
-}
-
-$api = new API;
-$api->processApi();
+// CALL: http://www.domain.com/services/API/hola/23/Miguel
+$cyber = new CyberREST("API");
+$cyber->get("/hola/{id}/{nombre}", function($app, $id, $nombre) {
+	$app->response(json_encode(array($id, $nombre)), 200);
+});
 
 ?>
